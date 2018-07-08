d81226a8a58acfd1a7b949cf8762d254eb623213
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-08 13:23:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees = []
+  attendees.each do
+    "Hello, my name is #{attendees}."
+  end
+end
+
+def assign_rooms
+  
+end
+
+def printer
+  
+end
