7da6b6eb8c57d76153d281e1aa5d33c3aebfb975
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-08 13:36:42 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index do |index|
+  attendees.each_with_index.map do |attendees, index|
 end
 
 def printer
